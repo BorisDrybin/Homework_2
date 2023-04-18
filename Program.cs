9dbae42d2b673955dd7116b56a18fdde158c5357
Boris Drybin <makoh93@gmail.com>
2023-04-18 13:55:34 +0300

    first task added

--- Program.cs
+++ Program.cs
@@ -29,4 +29,14 @@
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int result = num % 100;
+if(num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+while(num > 999)
+{
+    num = num /10;
+}
+num = num % 10;
+
+Console.WriteLine(num);
