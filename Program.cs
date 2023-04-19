9d56cb4b8bfd4dae56ac695fc10b880be4aa9499
Boris Drybin <makoh93@gmail.com>
2023-04-19 16:09:51 +0300

    third task added

--- Program.cs
+++ Program.cs
@@ -43,3 +43,29 @@
 //    num = num % 10;
 //    Console.WriteLine(num);
 // }
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num > 0 && num <= 7)
+// {
+//     if(num > 5)
+//     {
+//         Console.WriteLine("Выходной");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Будний день");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Это не день недели");
+// }
